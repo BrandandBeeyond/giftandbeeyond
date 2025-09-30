5b8b015d2fb254a3bc4959b5f25d5b8255e7f85e
dhiraj <dhirajmymodak22@gmail.com>
2025-09-30 18:33:45 +0530

    pushing for the day

--- src/app/admin/products/page.js
+++ src/app/admin/products/page.js
@@ -1,32 +1,32 @@
 "use client";
 
-import { fetchProducts } from '@/redux/actions/ProductAction';
-import { Pencil, Trash2, PlusCircle } from 'lucide-react'
-import { useEffect, useState } from 'react';
-import DataTable from 'react-data-table-component';
-import { useDispatch, useSelector } from 'react-redux';
-
+import { fetchProducts } from "@/redux/actions/ProductAction";
+import { Pencil, Trash2, PlusCircle } from "lucide-react";
+import { useEffect, useState } from "react";
+import DataTable from "react-data-table-component";
+import { useDispatch, useSelector } from "react-redux";
 
 const AdminProductsPage = () => {
   const dispatch = useDispatch();
-   const {products} = useSelector(state=>state.products);
+  const { products } = useSelector((state) => state.products);
 
-   const [formData, setFormData] = useState({
-      name:'',
-      description:'',
-      stock:'',
-      productSku:'',
-      price:'',
-      color:'',
-      images:[]
-   });
+  const [isModalOpen, setIsModalOpen] = useState(false);
+  const [formData, setFormData] = useState({
+    name: "",
+    description: "",
+    stock: "",
+    productSku: "",
+    price: "",
+    color: "",
+    images: [],
+  });
 
+  const handleModalOpen = () => setIsModalOpen(true);
+  const handleModalClose = () => setIsModalOpen(false);
 
-   
-
-   useEffect(()=>{
-       dispatch(fetchProducts());
-   },[dispatch]);
+  useEffect(() => {
+    dispatch(fetchProducts());
+  }, [dispatch]);
 
   const handleEdit = (id) => {
     alert(`Edit product with ID: ${id}`);
@@ -37,23 +37,23 @@ const AdminProductsPage = () => {
   };
   const columns = [
     {
-      name: 'Name',
-      selector: row => row.name,
+      name: "Name",
+      selector: (row) => row.name,
       sortable: true,
     },
     {
-      name: 'Price (₹)',
-      selector: row => row.price,
+      name: "Price (₹)",
+      selector: (row) => row.price,
       sortable: true,
     },
     {
-      name: 'Stock',
-      selector: row => row.stock,
+      name: "Stock",
+      selector: (row) => row.stock,
       sortable: true,
     },
     {
-      name: 'Actions',
-      cell: row => (
+      name: "Actions",
+      cell: (row) => (
         <div className="flex gap-2 justify-end">
           <button
             onClick={() => handleEdit(row.id)}
@@ -74,30 +74,39 @@ const AdminProductsPage = () => {
       ignoreRowClick: true,
       allowOverflow: true,
       button: true,
-    }
+    },
   ];
   return (
-    <div>
-      <div className="flex justify-between items-center mb-6">
-        <h1 className="text-2xl font-semibold">Products</h1>
-        <button className="flex items-center gap-2 bg-slate-900 hover:bg-slate-700 text-white cursor-pointer text-sm px-3 py-1.5 rounded-sm">
-          <PlusCircle size={18} />
-          Add Product
-        </button>
+    <>
+      <div>
+        <div className="flex justify-between items-center mb-6">
+          <h1 className="text-2xl font-semibold">Products</h1>
+          <button className="flex items-center gap-2 bg-slate-900 hover:bg-slate-700 text-white cursor-pointer text-sm px-3 py-1.5 rounded-sm" onClick={handleModalOpen}>
+            <PlusCircle size={18} />
+            Add Product
+          </button>
+        </div>
+
+        <div className="bg-white p-4 shadow-md rounded">
+          <DataTable
+            columns={columns}
+            data={products}
+            pagination
+            highlightOnHover
+            responsive
+          />
+        </div>
       </div>
 
-   <div className="bg-white p-4 shadow-md rounded">
-        <DataTable
-          columns={columns}
-          data={products}
-          pagination
-          highlightOnHover
-          responsive
-        />
-      </div>
-    </div>
+      {isModalOpen && (
+        <div className="fixed inset-0 bg-black/30 bg-opacity-50 flex items-center justify-center z-50">
+          <div className="bg-white p-6 rounded shadow-lg w-full max-w-md relative">
+            <h2 className="text-xl font-semibold mb-4">Add Product</h2>
+          </div>
+        </div>
+      )}
+    </>
   );
 };
-
 
 export default AdminProductsPage;
