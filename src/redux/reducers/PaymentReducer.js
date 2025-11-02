9dcf2da18e05618da03785587fa7d6f3c2bbcee2
dhiraj <dhirajmymodak22@gmail.com>
2025-11-02 21:19:59 +0530

    pushing from home

--- src/redux/reducers/PaymentReducer.js
+++ src/redux/reducers/PaymentReducer.js
@@ -1,10 +1,42 @@
-const initialState={
-     loading:false,
-     order:null,
-     paymentSuccess:false,
-     error:null
-}
+import {
+  PAYMENT_FAILURE,
+  PAYMENT_REQUEST,
+  PAYMENT_SUCCESS,
+} from "../constants/PaymentConstants";
 
-export const PaymentReducer=(state=initialState,action)=>{
-     
-}
+const initialState = {
+  loading: false,
+  order: null,
+  paymentSuccess: false,
+  error: null,
+};
+
+export const PaymentReducer = (state = initialState, action) => {
+  switch (action.type) {
+    case PAYMENT_REQUEST:
+      return {
+        ...state,
+        loading: true,
+        paymentSuccess: false,
+      };
+
+    case PAYMENT_SUCCESS:
+      return {
+        ...state,
+        loading: false,
+        order: action.payload,
+        paymentSuccess: true,
+      };
+
+    case PAYMENT_FAILURE:
+      return {
+        ...state,
+        loading: false,
+        error: action.payload,
+        paymentSuccess: false,
+      };
+
+    default:
+      return state;
+  }
+};
